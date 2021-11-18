8afe04e0104b1026d137146e51004b3bc13af26d
captainbboy <89534091+brett-k-cs@users.noreply.github.com>
2021-11-17 22:03:21 -0600

    fixed fall semester + top left grid being too big

--- scheduler/index.js
+++ scheduler/index.js
@@ -2,4 +2,4 @@ let headers = document.getElementsByClassName('header')
 let topLeft = document.getElementById('topLeft')
 let aDayHeaders = document.getElementsByClassName('aDay')
 
-topLeft.style.height = headers[1].clientHeight + aDayHeaders[0].clientHeight - 22 + "px"
+// topLeft.style.height = headers[1].clientHeight + aDayHeaders[0].clientHeight - 22 + "px"
