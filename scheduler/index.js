c9da6f0331974d9bc4c3d980001e6170cd2f2c8d
captainbboy <89534091+brett-k-cs@users.noreply.github.com>
2021-11-17 21:24:06 -0600

    Initial Version
    Has some styles, a cool select button screen, etc.

---
+++ scheduler/index.js
@@ -0,0 +1,5 @@
+let headers = document.getElementsByClassName('header')
+let topLeft = document.getElementById('topLeft')
+let aDayHeaders = document.getElementsByClassName('aDay')
+
+topLeft.style.height = headers[1].clientHeight + aDayHeaders[0].clientHeight - 22 + "px"
