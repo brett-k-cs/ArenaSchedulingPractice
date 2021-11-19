5ca6691527a67ff7390fd72e4e0d9aec3dfd0f23
captainbboy <89534091+brett-k-cs@users.noreply.github.com>
2021-11-18 21:50:21 -0600

    Added 'selected class', made it more functional, etc.
    also gave credit for home page in main html document

--- scheduler/index.js
+++ scheduler/index.js
@@ -20,6 +20,9 @@ classPicker.addEventListener("mousedown", chooseClass, false);
 let classes = {
     testClass: {
         avaliablePeriods: [0, 1, 2, 6, 7, 8]
+    },
+    "Principles of Engineering": {
+        avaliablePeriods: [2, 3, 5, 8]
     }
 }
 
@@ -35,13 +38,30 @@ for (let i = 0; i < Object.keys(classes).length; i++) {
     classPicker.appendChild(div)
 }
 
+let selectedClass = null;
+
 function chooseClass(event) {
-    if(event.target.id && classes[event.target.id]) {
-        let classInfo = classes[event.target.id]
+    if((event.target.nodeName == "SPAN" && event.target?.parentElement?.id && classes[event.target.parentElement.id]) || event.target.id && classes[event.target.id]) {
+        let classInfo = classes[event.target.id] || classes[event.target.parentElement.id]        
         let avaliablePeriods = classInfo.avaliablePeriods
-        for (let l = 0; l < avaliablePeriods.length; l++) {
-            highLightClasses(avaliablePeriods[l])
+        var curSelectedClass = event.target.id || event.target.parentElement.id
+
+        for (let i = 0; i < classDivs.length; i++) {
+            if(classDivs[i].classList.contains('highlighted')) classDivs[i].classList.remove('highlighted')
         }
+
+        if(selectedClass != curSelectedClass) {
+            for (let l = 0; l < avaliablePeriods.length; l++) {
+                highLightClasses(avaliablePeriods[l])
+            }
+            selectedClass = curSelectedClass;
+        } else selectedClass = null
+
+        let previousSelectedClass = document.getElementsByClassName('highlightedClass')[0]
+        if(previousSelectedClass && previousSelectedClass.id != curSelectedClass) previousSelectedClass.classList.toggle('highlightedClass')
+
+        if(event.target.nodeName == "DIV") event.target.classList.toggle('highlightedClass')
+        else event.target.parentElement.classList.toggle('highlightedClass')
     }
 }
 
@@ -49,7 +69,9 @@ function highLightClasses(period) {
     let day = period == 0 || period == 1 || period == 8 ? "both" : "aDay"
     if(day == "aDay" && (period == 5 || period == 6 || period == 7)) {
         day = "bDay"
-        period -= 4
+        period -= 3
+    } else if (period == 8) {
+        period -= 3
     }
     for (let i = 0; i < 4; i++) {
         if(day == 'both' || (day == 'aDay' && i % 2 == 0) || (day == 'bDay' && i % 2 == 1)) {
@@ -57,4 +79,5 @@ function highLightClasses(period) {
             element.classList.add('highlighted')
         }
     }
+
 }
